d15fde4a89a74beb16986188c0b66592a74b360e
Lalitha Siddardha Tadavarti <89541762+Tadavarti@users.noreply.github.com>
2021-12-18 16:48:36 -0700

    Add files via upload

---
+++ homework-0-Tadavarti-main/operators.py
@@ -0,0 +1,31 @@
+def add(a, b):
+    """
+    Adding two numbers
+
+    Args:
+        a (integer): a number
+        b (integer): a number
+
+    Returns:
+        a + b
+
+    (You can implement this with one line of code, just use the + operator)
+    """
+    return(a+b)
+    raise NotImplementedError
+
+
+def sort(a):
+    """
+    Sorting a list
+
+    Args
+        a (list): a list
+
+    Returns:
+        A new list with the elements of a sorted increasingly
+
+    (You can implement this one line of code. You do not need to implement your own sort method)
+    """
+    return sorted(a)
+    raise NotImplementedError
