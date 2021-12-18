2c6792adcb3e7b4e5a4d21d068e4dd7865e43fca
Lalitha Siddardha Tadavarti <89541762+Tadavarti@users.noreply.github.com>
2021-12-18 16:56:02 -0700

    Add files via upload

---
+++ homework-3-minitorch-module-2-Tadavarti-master/README.md
@@ -0,0 +1,48 @@
+# MiniTorch Module 2
+
+<img src="https://minitorch.github.io/_images/match.png" width="100px">
+
+
+* Docs: https://minitorch.github.io/
+
+* Overview: https://minitorch.github.io/module2.html
+
+This assignment requires the following files from the previous assignments.
+
+        minitorch/operators.py minitorch/module.py minitorch/autodiff.py minitorch/scalar.py minitorch/module.py project/run_manual.py project/run_scalar.py
+# Simple_Graph_Plots:
+## Simple_DataSet:
+![simpe_Dataset](https://user-images.githubusercontent.com/89541762/137572967-ad11ca10-b26d-4780-8c9f-fcceedf4ee0f.png)
+## Simple_DataSet_Model:
+![simple_model](https://user-images.githubusercontent.com/89541762/137572999-479e1f6d-ff73-4b49-8cf1-80d9bb307f6b.png)
+## Simple_DataSet_Training:
+![simple_Hyperparameters](https://user-images.githubusercontent.com/89541762/137573014-e5146efb-59d6-4392-9995-e26dce3aca94.png)
+## Simple_Dataset_LossGraph:
+![simple_loss_graph](https://user-images.githubusercontent.com/89541762/137573031-ea6d50fd-1e4e-4889-8988-0c259903a724.png)
+# Diagonal_Graph_Plots:
+## Diagonal_DataSet:
+![diag_Dataset](https://user-images.githubusercontent.com/89541762/137573052-38be0358-5ca8-4799-9b35-e5e1648e5d1c.png)
+## Diagonal_DataSet_Model
+![diag_Model](https://user-images.githubusercontent.com/89541762/137573066-87206a21-ae3e-40a3-8c15-ad6c5246b188.png)
+## Diagonal_DataSet_Training:
+![diag_Hyperparameters](https://user-images.githubusercontent.com/89541762/137573081-2dce9662-209a-4e64-9a40-f83110fc9f45.png)
+## Diagonal_DataSet_LossGraph:
+![diag_loss_graph](https://user-images.githubusercontent.com/89541762/137573095-733e3422-136a-4f16-bafc-39440ba28b97.png)
+# Split_Graph_Plots:
+## Split_Dataset:
+![split_Dataset](https://user-images.githubusercontent.com/89541762/137573112-bb45cf5a-8821-4fb7-b361-7f8496841e49.png)
+## Split_dataset_Model:
+![split_Model](https://user-images.githubusercontent.com/89541762/137573116-ff529e99-7b45-4668-aa92-06aa7365531d.png)
+## Split_dataset_Training:
+![split_Hyperparameters](https://user-images.githubusercontent.com/89541762/137573128-8f135cc9-374d-4c48-8f88-b9fbd955c213.png)
+## Split_Dataset_LossGraph:
+![split_loss_graph](https://user-images.githubusercontent.com/89541762/137573181-a3f1ca16-c7d6-4b58-add5-8222a24f7d7b.png)
+# XOR_Graph_Plots:
+## XOR_DataSet:
+![XOR_Dataset](https://user-images.githubusercontent.com/89541762/137573187-14e70c15-c9e7-41a5-ab54-5d9bf6e54bd8.png)
+## XOR_DataSet_Model:
+![XOR_Model](https://user-images.githubusercontent.com/89541762/137573193-932d4154-eb09-4bb8-ab02-153f9e17db89.png)
+## XOR_DataSet_Training:
+![XOR_Hyperparameters](https://user-images.githubusercontent.com/89541762/137573198-9b2fb36f-11d1-4b3e-88f2-a27367bf550f.png)
+## XOR_DataSet_LossGraph:
+![XOR_loss_Graph](https://user-images.githubusercontent.com/89541762/137573206-b5e50077-8920-4523-8028-9fbecdc85811.png)
---
+++ homework-3-minitorch-module-2-Tadavarti-master/minitorch/operators.py
@@ -0,0 +1,179 @@
+"""
+Collection of the core mathematical operators used throughout the code base.
+"""
+
+
+import math
+
+# ## Task 0.1
+
+# Implementation of a prelude of elementary functions.
+
+
+def mul(x, y):
+    ":math:`f(x, y) = x * y`"
+    # TODO: Implement for Task 0.1.
+    return float(x) * float(y)
+    raise NotImplementedError('Need to implement for Task 0.1')
+
+
+def id(x):
+    ":math:`f(x) = x`"
+    # TODO: Implement for Task 0.1.
+    return x
+    raise NotImplementedError('Need to implement for Task 0.1')
+
+
+def add(x, y):
+    ":math:`f(x, y) = x + y`"
+    # TODO: Implement for Task 0.1.
+    return float(x) + float(y)
+    raise NotImplementedError('Need to implement for Task 0.1')
+
+
+def neg(x):
+    ":math:`f(x) = -x`"
+    # TODO: Implement for Task 0.1.
+    return (-1.0 * float(x))
+    raise NotImplementedError('Need to implement for Task 0.1')
+
+
+def lt(x, y):
+    ":math:`f(x) =` 1.0 if x is less than y else 0.0"
+    # TODO: Implement for Task 0.1.
+    return (1 if x < y else 0)
+    raise NotImplementedError('Need to implement for Task 0.1')
+
+
+def eq(x, y):
+    ":math:`f(x) =` 1.0 if x is equal to y else 0.0"
+    # TODO: Implement for Task 0.1.
+    return(1 if x == y else 0)
+    raise NotImplementedError('Need to implement for Task 0.1')
+
+
+def max(x, y):
+    ":math:`f(x) =` x if x is greater than y else y"
+    # TODO: Implement for Task 0.1.
+    return(x if x > y else y)
+    raise NotImplementedError('Need to implement for Task 0.1')
+
+
+def is_close(x, y):
+    ":math:`f(x) = |x - y| < 1e-2` "
+    # TODO: Implement for Task 0.1.
+    return(abs(float(x) - float(y)) < 1e-2)
+    raise NotImplementedError('Need to implement for Task 0.1')
+
+
+def sigmoid(x):
+    # TODO: Implement for Task 0.1.
+    return((1.0 / (1.0 + exp(-x))) if x > 0 else (exp(x) / (1 + exp(x))))
+    raise NotImplementedError('Need to implement for Task 0.1')
+
+
+def relu(x):
+    return(x if x > 0.0 else 0.0)
+    raise NotImplementedError('Need to implement for Task 0.1')
+
+
+EPS = 1e-6
+
+
+def log(x):
+    ":math:`f(x) = log(x)`"
+    return math.log(x + EPS)
+
+
+def exp(x):
+    ":math:`f(x) = e^{x}`"
+    return math.exp(x)
+
+
+def log_back(x, d):
+    r"If :math:`f = log` as above, compute d :math:`d \times f'(x)`"
+    # TODO: Implement for Task 0.1.
+    return(d * (1 / x))
+    raise NotImplementedError('Need to implement for Task 0.1')
+
+
+def inv(x):
+    ":math:`f(x) = 1/x`"
+    # TODO: Implement for Task 0.1.
+    return(1.0 / x)
+    raise NotImplementedError('Need to implement for Task 0.1')
+
+
+def inv_back(x, d):
+    r"If :math:`f(x) = 1/x` compute d :math:`d \times f'(x)`"
+    # TODO: Implement for Task 0.1.
+    return(-d * (1 / (x * x)))
+    raise NotImplementedError('Need to implement for Task 0.1')
+
+
+def relu_back(x, d):
+    r"If :math:`f = relu` compute d :math:`d \times f'(x)`"
+    # TODO: Implement for Task 0.1.
+    return(d if x > 0 else 0)
+    raise NotImplementedError('Need to implement for Task 0.1')
+
+
+# ## Task 0.3
+
+# Small library of elementary higher-order functions for practice.
+
+def map(fn , l):
+    for i in range(len(l)):
+        if(l[i]) < 0:
+            l[i] = fn(str(l[i]))
+        else:
+            l[i] = fn(str(l[i]))
+    return l
+    raise NotImplementedError('Need to implement for Task 0.3')
+
+
+def negList(ls):
+    ls = list(map(neg , ls))
+    return ls
+    raise NotImplementedError('Need to implement for Task 0.3')
+
+
+def zipWith(fn , l1 , l2):
+    for i in range(len(l1)):
+        l1[i] = fn(str(l1[i]) , str(l2[i]))
+    return l1
+    raise NotImplementedError('Need to implement for Task 0.3')
+
+
+def addLists(ls1, ls2):
+    "Add the elements of `ls1` and `ls2` using :func:`zipWith` and :func:`add`"
+    # TODO: Implement for Task 0.3.
+    result = ls1
+    result = list(zipWith(add , ls1 , ls2))
+    return result
+    raise NotImplementedError('Need to implement for Task 0.3')
+
+
+def reduce(fn, start):
+    if(len(start) == 0):
+        if(fn == mul):
+            return (str(1))
+        return (0)
+    if(len(start) != 1):
+        return (fn(reduce(fn , start[1:]) , str(start[0])))
+    else:
+        return (fn(reduce(fn , []) , str(start[0])))
+
+
+def sum(ls):
+    "Sum up a list using :func:`reduce` and :func:`add`."
+    # TODO: Implement for Task 0.3.
+    return reduce(add , ls)
+    raise NotImplementedError('Need to implement for Task 0.3')
+
+
+def prod(ls):
+    "Product of a list using :func:`reduce` and :func:`mul`."
+    # TODO: Implement for Task 0.3.
+    return reduce(mul , ls)
+    raise NotImplementedError('Need to implement for Task 0.3')
